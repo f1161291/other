4dbe29a24c52bca33c900f88543f0fa6426e7f71
f1161291 <78488261+f1161291@users.noreply.github.com>
2026-08-07 08:56:12 +0800

    Update dd.sh

--- dd.sh
+++ dd.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-curl -O https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh || \
-wget -O reinstall.sh https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh
+URL="https://raw.githubusercontent.com/bin456789/reinstall/main/reinstall.sh"
 
-echo "请选择系统："
+# 下载脚本
+curl -LO "$URL" || wget -O reinstall.sh "$URL" || {
+    echo "下载失败！"
+    exit 1
+}
+
+echo "请选择要重装的系统："
+
+PS3="请输入序号 [1-7]: "
+
 select os in debian13 debian12 debian11 ubuntu24.04 ubuntu22.04 centos9 fnos; do
-    [ -n "$os" ] && break
+    if [ -n "$os" ]; then
+        break
+    else
+        echo "请输入正确的序号！"
+    fi
 done
 
+echo "正在重装：$os"
 bash reinstall.sh "$os"
